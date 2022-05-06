4b64173a0d466110494085e544d92929a0695c63
diegolagunax <diegolagunam@gmail.com>
2022-05-06 17:45:59 -0300

    Criação das tabelas do banco a partir das entidades

---
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -0,0 +1,60 @@
+package com.algaworks.algafood.domain.model;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Entity
+public class Cozinha {
+	
+	@Id
+	private Long id;
+	
+	@Column(name= "nom_cozinha")
+	private String nome;
+	
+	
+	
+	//get and setters
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	
+	//Equals and Hashcode
+	
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, nome);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cozinha other = (Cozinha) obj;
+		return Objects.equals(id, other.id) && Objects.equals(nome, other.nome);
+	}
+
+ 
+	
+	
+
+}
---
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -0,0 +1,82 @@
+package com.algaworks.algafood.domain.model;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Restaurante {
+	
+	
+	//ATRIBUTOS DA CLASSE
+	@Id
+	private Long id;
+	
+	@Column(name= "nom_restaurante")
+	private String Nome;
+	
+	@Column(name= "taxa_frete")
+	private BigDecimal taxaFrete;
+	
+	
+	//GET AND SET
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return Nome;
+	}
+
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+
+	public BigDecimal getTaxaFrete() {
+		return taxaFrete;
+	}
+
+	public void setTaxaFrete(BigDecimal taxaFrete) {
+		this.taxaFrete = taxaFrete;
+	}
+	
+	
+	//EQUALS AND HASHCODEV
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Nome, id, taxaFrete);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Restaurante other = (Restaurante) obj;
+		return Objects.equals(Nome, other.Nome) && Objects.equals(id, other.id)
+				&& Objects.equals(taxaFrete, other.taxaFrete);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
